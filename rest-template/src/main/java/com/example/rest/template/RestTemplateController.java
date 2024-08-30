40877fa86aa8a7d86b2862f43186bff67a04ae83
corina <valeriabalan1997@gmail.com>
2024-08-30 21:31:21 +0300

    creating new functionalities

--- rest-template/src/main/java/com/example/rest/template/RestTemplateController.java
+++ rest-template/src/main/java/com/example/rest/template/RestTemplateController.java
@@ -1,2 +1,24 @@
-package com.example.rest.template;public class RestTemplateController {
+package com.example.rest.template;
+
+
+import com.example.rest.template.service.RestTemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/restTemplate")
+public class RestTemplateController {
+
+    @Autowired
+    RestTemplateService restTemplateService;
+
+    @GetMapping("/{id}")
+    public String getTemplate(@PathVariable(name="id") int id){
+      return  restTemplateService.getPost(id);
+    }
+
+    @PostMapping()
+    public String postTemplate(){
+        return restTemplateService.createPost();
+    }
 }
--- rest-template/src/main/java/com/example/rest/template/service/RestTemplateService.java
+++ rest-template/src/main/java/com/example/rest/template/service/RestTemplateService.java
@@ -2,8 +2,12 @@ package com.example.rest.template.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class RestTemplateService {
@@ -11,5 +15,31 @@ public class RestTemplateService {
     @Autowired
     private RestTemplate restTemplate;
 
-    
+
+    //GET
+    public String getPost(int id){
+        String url="https://jsonplaceholder.typicode.com/posts/"+id;
+        return restTemplate.getForObject(url,String.class);
+    }
+
+
+    //POST
+    public String createPost(){
+        String url="https://jsonplaceholder.typicode.com/posts";
+
+        Map<String,String> request=new HashMap<>();
+        request.put("title","foo");
+        request.put("body","bar");
+        request.put("userId","1");
+
+        return restTemplate.postForObject(url,request,String.class);
+    }
+
+    //PUT
+    public ResponseEntity<String> getPostEntity(int id){
+        String url="https://jsonplaceholder.typicode.com/posts/"+id;
+        return restTemplate.getForEntity(url,String.class);
+    }
+
+
 }
